bd51bd3ae74a99093700cd307cf6d0a1f1b955b3
Kannan <kannabirank@yahoo.com>
2015-02-25 23:35:11 +1000

    new additions
    new additions

---
+++ JSOM Samples/04. Create List Item.js
@@ -0,0 +1,51 @@
+﻿<script type="text/javascript" language="javascript" src="http://spdev2013/QLDConnect/SiteAssets/jquery-1.10.2.min.js"></script>
+<script type="text/javascript" language="javascript">
+    
+    function createListItem()
+    {
+        //load script on demand
+        SP.SOD.executeFunc('SP.js','SP.ClientContext', function() {
+            //do something
+            var clientContext;
+            var oWebsite;
+            var oList;
+            var itemCreateInfo;
+
+            clientContext = new SP.ClientContext.get_current();
+            oWebsite = clientContext.get_web();
+            oList = oWebsite.get_lists().getByTitle("Announcements");
+
+            itemCreateInfo = new SP.ListItemCreationInformation();
+            this.oListItem = oList.addItem(itemCreateInfo);
+            this.oListItem.set_item("Title", "My New Item!");
+            this.oListItem.set_item("Body", "Hello World!");
+            this.oListItem.update();
+    
+            clientContext.load(this.oListItem);
+            clientContext.executeQueryAsync(
+                Function.createDelegate(this, successHandler),
+                Function.createDelegate(this, errorHandler)
+            );
+            function successHandler() {
+                resultpanel.innerHTML = "Go to the <a href='../Lists/Announcements'>list</a> to \
+                        see your new item. Item id is " + this.oListItem.get_id() + " and \
+                        Item title is " + this.oListItem.get_item('Title');
+            }
+            function errorHandler() {
+                resultpanel.innerHTML = "Request failed: " + arguments[1].get_message();
+            }
+        });
+    }
+
+    $(document).ready(function() {
+	
+	    //load script on demand
+	    SP.SOD.executeFunc('SP.js','SP.ClientContext', function() {
+	        //do something
+	    });
+    });
+
+</script>
+<H1>Create List Item</H1>
+<input onclick="createListItem()" name="createItem" type="button" value="Create List Item"></input>
+<div id="resultpanel"></div>
---
+++ JSOM Samples/Client Side Rendering/DiscussionList.js
@@ -0,0 +1,34 @@
+﻿<script type="text/javascript" language="javascript" src="http://spdev2013/QLDConnect/SiteAssets/jquery-1.10.2.min.js"></script>
+<script type="text/javascript">
+(function () {
+    if (typeof SPClientTemplates === 'undefined')
+        return;
+
+    var listCtx = {};
+
+    listCtx.Templates = {};
+    listCtx.OnPostRender = method1;
+
+    /*
+    listCtx.Templates.Fields = {
+        'RiskRating': {
+            'View': riskRatingView,
+            'NewForm': riskRatingNewForm
+        }
+    };*/
+
+    SPClientTemplates.TemplateManager.RegisterTemplateOverrides(listCtx);
+
+})();
+
+function method1(ctx)
+{
+    var x = ctx.ListTitle;
+    if (x == "Discussions List")
+    {
+        alert(ctx.ListData.Row[0].ID);
+        alert($('#commandBar0-going-link').text());
+    }
+
+}
+</script>
